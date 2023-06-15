a1d7f085a709507fc1afa673b8dec5c1e2a7f55d
Dio Vieira <dio.vieira@engie.com>
2023-06-15 12:24:28 -0300

    Aula152 - Solução Ruim

---
+++ Aula152/Program.cs
@@ -0,0 +1,23 @@
+﻿using Aula152.Entities;
+using System;
+
+namespace Aula152
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Room number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+
+            Console.WriteLine("Reservation: " + reservation.ToString());
+        }
+    }
+}
